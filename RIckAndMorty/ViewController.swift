f1f5fc479c689f551a29c9de60dda3177f9d2b1d
Gabriel <gb953132@gmail.com>
2023-04-29 11:23:15 -0300

    Initial Commit

---
+++ RIckAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIckAndMorty
+//
+//  Created by Gabriel Ramos on 29/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
